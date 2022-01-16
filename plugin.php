6e2319a578a8d281617f2eb13fb90548952f0bf7
jschwartz <jschwartz@emfluence.com>
2022-01-16 14:21:15 -0600

    Refinements pre-release: - Change name of plugin, - Add readme, - Add an admin notice if Wordfence isn't installed or if this plugin has never run.

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Wordfence Cloudfront IP Address Updater
+Plugin Name: Wordfence Cloudfront Proxy IP Address Updater
 Plugin URI: https://github.com/emfluencekc
 Description: If you have Cloudfront in front of Wordpress and are using Wordfence, this plugin is for you. Automatically update the proxy IP addresses for Cloudfront in Wordfence, so that Wordfence can correctly identify the end user IP address. Cloudfront updates its list of IP addresses every now and then.
 Author: emfluence
@@ -9,25 +9,29 @@ Author URI: https://emfluence.com
 Network: true
 Requires at least: 5.0
 Requires PHP: 5.6
+License: GPLv2 or later
 */
 
 class Wordfence_Cloudfront_IP_Updater {
 
 	protected $cron_name = 'wfcfipu_cron_hook';
+	protected $cloudfront_ip_addresses_url = 'http://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips';
 
 	function __construct() {
 		if ( ! wp_next_scheduled( $this->cron_name ) ) {
 			wp_schedule_event( time(), 'daily', $this->cron_name );
 		}
 		add_action($this->cron_name, [$this, 'refresh_ips']);
+		register_activation_hook(__FILE__, [$this, 'refresh_ips']);
 		register_deactivation_hook( __FILE__, [$this, 'deactivate'] );
+		add_action('admin_notices', [$this, 'check_wordfence_installed']);
 	}
 
 	function refresh_ips() {
 		if(!defined('WORDFENCE_VERSION')) return;
 
 		//download list of IPs
-		$ips = file_get_contents('http://d7uri8nf7uskq.cloudfront.net/tools/list-cloudfront-ips');
+		$ips = file_get_contents($this->cloudfront_ip_addresses_url);
 		$ips = json_decode($ips, true);
 		$ips = implode("\n", array_merge($ips['CLOUDFRONT_GLOBAL_IP_LIST'], $ips['CLOUDFRONT_REGIONAL_EDGE_IP_LIST']));
 
@@ -35,11 +39,22 @@ class Wordfence_Cloudfront_IP_Updater {
 		$table = wfDB::networkTable('wfConfig');
 		$wpdb->query($wpdb->prepare("UPDATE %s SET val = %s WHERE name = 'howGetIPs_trusted_proxies'", $table, $ips));
 		wp_cache_delete('alloptions', 'wordfence');
+		update_option('wfcfipu_last_ran', time(), false);
 	}
 
 	function deactivate() {
 		$timestamp = wp_next_scheduled( $this->cron_name );
 		wp_unschedule_event( $timestamp, $this->cron_name );
+	}
+
+	function check_wordfence_installed() {
+		if(!defined('WORDFENCE_VERSION')) {
+			_e('<div class="notice notice-error"><p><i>Wordfence Cloudfront Proxy IP Address Updater</i> requires <i>Wordfence</i> to be installed</p></div>');
+			return;
+		}
+		if(false === get_option('wfcfipu_last_ran', false)) {
+			_e('<div class="notice notice-error"><p><i>Wordfence Cloudfront Proxy IP Address Updater</i> has not yet been able to get Cloudfront proxy IP addresses</p></div>');
+		}
 	}
 
 }
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Wordfence Cloudfront Proxy IP Address Updater ===
+Tags: security, wordfence, cloudfront, proxy
+Requires at least: 5.0
+Tested up to: 5.8.3
+Requires PHP: 5.6
+Stable tag: 1.0
+Contributors: emfluencekc, mightyturtle
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automatically update Wordfence's list of proxy IP addresses with Cloudfront IP addresses
+
+== Description ==
+
+If you have Cloudfront in front of Wordpress and are using Wordfence, this plugin is for you.
+
+If you don't provide Cloudfront's IP addresses to Wordfence, any bad behavior out there can get Cloudfront itself blocked by Wordfence - and then no one will be able to access your site.
+
+This plugin addds and automatically updates the proxy IP addresses for Cloudfront in Wordfence, so that Wordfence can correctly identify the end user's IP address.
+
+Cloudfront updates its list of IP addresses every now and then. Don't manually add IP addresses, and then try to keep track of Cloudfront IP address changes. Just install this plugin and stop worrying!
+
+Want to change how this plugin works, or add to it? Fork it on GitHub!
+https://github.com/emfluencekc/Wordfence-Cloudfront-IPs
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/wordfence-cloudfront-ips` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+3. That's it. This plugin has no settings page, and work with both individual and network setups.
+
+== Changelog ==
+
+= 1.0 =
+Initial release. Install this plugin and your proxy IP addresses are taken care of.
